ee29664eb63743b5c0424cb2a065c6188d8edb87
GarrettT <spinnytea@gmail.com>
2026-01-17 17:17:16 -0500

    document a desired feature

--- src/game/move/move.findAvailableMoves.test.ts
+++ src/game/move/move.findAvailableMoves.test.ts
@@ -212,4 +212,118 @@ describe('game/move.findAvailableMoves', () => {
 			});
 		});
 	});
+
+	describe('deprecated: allow foundation to move when collapse', () => {
+		test('$touchAndMove(4D) ⇒ move 4d 4D→cell', () => {
+			const game = FreeCell.parse(
+				'' +
+					' 5H 3S JC    3H AS 3C 4D \n' +
+					' KS 9H 2S 7H QC QS TD KH \n' +
+					'    8S 5C    JD QD 9C    \n' +
+					'       4S    6S TC       \n' +
+					'       4C    JH KC       \n' +
+					'       KD    TS QH       \n' +
+					'       6H    9D JS       \n' +
+					'       8D    8C TH       \n' +
+					'       7C    7D 9S       \n' +
+					'       6D    6C 8H       \n' +
+					'       5S    5D 7S       \n' +
+					'       4H                \n' +
+					' move 4h 4D→3D\n' +
+					':h shuffle32 21388\n' +
+					' 65 53 65 46 73 4a 24 2b \n' +
+					' 82 83 b3 8b 84 78 73 57 \n' +
+					' 25 75 27 46 1b 1c ch 2h \n' +
+					' 12 1c 16 41 4h '
+			);
+			expect(game.history.length).toBe(31);
+
+			const gameUndid = game.undo();
+			expect(availableMovesMinimized(gameUndid.$selectCard('4D').availableMoves, true)).toEqual([
+				['d', 'cell', -1], // this is a possible move
+				['h⡃', 'foundation', 1], // we pick this one as the best option (fair)
+			]);
+			// if we move it specifically, this is the resulting text (a bit redundant of a test)
+			expect(gameUndid.moveByShorthand('4d').previousAction.text).toBe('move 4d 4D→cell');
+
+			// now, we don't allow this card to be selected, so there are no available moves
+			expect(game.$selectCard('4D').selection).toBe(null);
+			expect(game.$selectCard('4D').availableMoves).toBe(null);
+			// and if we force it… the selection is peekOnly
+			expect(game.$selectCard('4D', { allowSelectFoundation: true }).selection).toEqual({
+				location: { fixture: 'foundation', data: [3] },
+				cards: [{ rank: '4', suit: 'diamonds', location: { fixture: 'foundation', data: [3] } }],
+				peekOnly: true,
+			});
+			// and the foundation doesn't have any "depth", so we can't put the cursor on a different card
+			// e.g. if we were to make 3D selection peekOnly, but allow 4D selection to move
+			expect(game.$selectCard('3D', { allowSelectFoundation: true }).selection).toEqual({
+				location: { fixture: 'foundation', data: [3] },
+				cards: [{ rank: '4', suit: 'diamonds', location: { fixture: 'foundation', data: [3] } }],
+				peekOnly: true,
+			});
+			expect(
+				availableMovesMinimized(
+					game.$selectCard('4D', { allowSelectFoundation: true }).availableMoves,
+					true
+				)
+			).toEqual([]);
+
+			// in our current implementation, this is how it is
+			expect(game.$touchAndMove('4D').previousAction).toEqual({
+				text: 'touch stop',
+				type: 'invalid',
+			});
+
+			// this is a nice to have
+			// but we have to start bending a lot of rule, and start blocking them elsewhere
+			// (selecting the foundation is peekOnly for good reason)
+			// (the position 'h' represents all foundation positions, we shouldn't also add depth)
+			expect(game.$touchAndMove('4D').previousAction).not.toEqual({
+				text: 'move 4d 4D→cell',
+				type: 'move',
+			});
+		});
+
+		test('$touchAndMove(3H) ⇒ move 1c 3H→cell (auto-foundation 133 AD,2D,3C)', () => {
+			const game = FreeCell.parse(
+				'' +
+					' 5H 3S       3H AS 3C 2D \n' +
+					' 8H 9H 2S 7H QC QS TD KH \n' +
+					' 7S 3D 5C 4D JD QD 9C    \n' +
+					' JC    4S KS 6S TC       \n' +
+					' 8S    4C    JH KC       \n' +
+					'       KD    TS QH       \n' +
+					'       6H    9D JS       \n' +
+					'       8D    8C TH       \n' +
+					'       7C    7D 9S       \n' +
+					'       6D    6C          \n' +
+					'       5S    5D          \n' +
+					'       4H                \n' +
+					' move 1h 3H→2H (auto-foundation 133 AD,2D,3C)\n' +
+					':h shuffle32 21388\n' +
+					' 65 53 65 46 73 4a 24 2b \n' +
+					' 82 83 b3 8b 84 78 73 57 \n' +
+					' 25 75 27 46 1b 1h '
+			);
+			expect(game.history.length).toBe(24);
+
+			const gameUndid = game.undo();
+			expect(availableMovesMinimized(gameUndid.$selectCard('3H').availableMoves, true)).toEqual([
+				['c', 'cell', -1], // this is a possible move
+				['d', 'cell', -1],
+				['h⡀', 'foundation', 4], // we pick this one as the best option (fair)
+			]);
+			// if we move it specifically, this is the resulting text (a bit redundant of a test)
+			expect(gameUndid.moveByShorthand('1c').previousAction.text).toBe(
+				'move 1c 3H→cell (auto-foundation 133 AD,2D,3C)'
+			);
+
+			// now this would just be asking for too much
+			expect(game.$touchAndMove('3H').previousAction).not.toEqual({
+				text: 'move 1c 3H→cell (auto-foundation 133 AD,2D,3C)',
+				type: 'move',
+			});
+		});
+	});
 });
